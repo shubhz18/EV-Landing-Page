1a250b24ae622bf629556a38388bc67830dac529
Shubham Phale <shubhamphale123@gmail.com>
2025-01-04 15:12:42 +0530

    add

--- src/Components/BackGround/BackGround.jsx
+++ src/Components/BackGround/BackGround.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import './Background.css';
-import video1 from '../../Assets/video1.mp4';
+import video1 from '/video1.mp4';
 import image1 from '../../Assets/image1.png';
 import image2 from '../../Assets/image2.png';
 import image3 from '../../Assets/image3.png';
